7d767b3f50817d0599ecf4b02db3eef1561025a7
Antoni Górski <antoni.gorski@int.pl>
2021-02-01 21:16:17 +0100

    basic layout

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import { route } from '../router';
+import homeController from './controllers/home';
+import page404Controller from './controllers/page404';
 
-route('/', 'home', () => {});
+route('/', 'home', homeController);
 
-route('*', 'page404', () => {});
+route('*', 'page404', page404Controller);
--- src/templates/home.js
+++ src/templates/home.js
@@ -1,5 +1,10 @@
 <script type="text/html" id="home">
 	<div class="home">
-		<span>Start</span>
+		<div class="form-container">
+			<div class="some-guy-container">
+				<img class="some-guy" src="assets/images/some-guy.jpg"/>
+			</div>
+			<span>aaa</span>
+		</div>
 	</div>
 </script>
