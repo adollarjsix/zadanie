972981d84cd7b9105ec314a64250a5ffd3aee160
Antoni Górski <antoni.gorski@int.pl>
2021-02-01 19:43:27 +0100

    gulp build & serve & watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const less = require('gulp-less');
+const webpack = require('webpack-stream');
+const connect = require('gulp-connect');
+
+function webpackTask() {
+  return gulp.src('src/app.js')
+    .pipe(webpack(require('./webpack.config.js')))
+	.pipe(gulp.dest('.'))
+	.pipe(connect.reload());
+}
 
 function indexTask() {
 	return gulp.src([
-		'index_partials/index-top.html',
-		'index_partials/index-head.html',
+		'src/index_partials/index-top.html',
+		'src/index_partials/index-head.html',
 		'src/templates/*.js',
-		'index_partials/index-bottom.html'
+		'src/index_partials/index-bottom.html'
 	])
 	.pipe(concat('index.html'))
-	.pipe(gulp.dest('.'));
+	.pipe(gulp.dest('.'))
+	.pipe(connect.reload());
 }
 
 function stylesTask() {
@@ -18,9 +28,31 @@ function stylesTask() {
 	.pipe(less())
 	.pipe(concat('styles.css'))
 	.pipe(gulp.dest('.'))
+	.pipe(connect.reload());
 }
 
-exports.default = gulp.series(
+const build = gulp.series(
+	webpackTask,
 	indexTask,
 	stylesTask
+);
+
+const serve = async function() {
+	connect.server({
+		livereload: true
+	});
+};
+
+const watch = async function() {
+	gulp.watch('src/**/*.*', build);
+};
+
+exports.build = build;
+exports.serve = serve;
+exports.watch = watch;
+
+exports.default = gulp.series(
+	build,
+	serve,
+	watch
 );
