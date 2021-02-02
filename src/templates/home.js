8abb244f2f9a0c105602b828059c64b5eb8e1f07
Antoni Górski <antoni.gorski@int.pl>
2021-02-02 04:21:36 +0100

    form layout and styles

--- src/templates/home.js
+++ src/templates/home.js
@@ -4,7 +4,17 @@
 			<div class="some-guy-container">
 				<img class="some-guy" src="assets/images/some-guy.jpg"/>
 			</div>
-			<span>aaa</span>
+			<form>
+				<h1>Hi,</h1>
+				<h2>Zwolnieni z Teorii!</h2>
+				<label for="username">Nazwa użytkownika:</label>
+				<input type="text" id="username" name="username"></input>
+				<label for="password">Hasło:</label>
+				<input type="password" id="password" name="password"></input>
+				<input type="submit" value="Wyślij"></input>
+				<span class="flex"></span>
+				<p class="where">Surprise? Where? =)</p>
+			</form>
 		</div>
 	</div>
 </script>
