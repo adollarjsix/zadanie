aec4823b9e06a996cca2cf07f5aee7ee6ab4c979
Antoni Górski <antoni.gorski@int.pl>
2021-02-02 08:58:38 +0100

    logged in page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import { route } from '../router';
 import homeController from './controllers/home';
+import loggedInController from './controllers/loggedin';
 import page404Controller from './controllers/page404';
 
 route('/', 'home', homeController);
+
+route('/loggedin', 'loggedin', loggedInController);
 
 route('*', 'page404', page404Controller);
---
+++ src/templates/loggedin.js
@@ -0,0 +1,6 @@
+<script type="text/html" id="loggedin">
+	<div class="logged-in">
+		<h1>Success!</h1>
+        <h2>You are successfully logged in =)</h2>
+	</div>
+</script>
