0209f2627c7fad52ffb0b39287b7069f8aa8dc81
Antoni Górski <antoni.gorski@int.pl>
2021-02-02 10:42:42 +0100

    better logged in styling

--- src/templates/loggedin.js
+++ src/templates/loggedin.js
@@ -1,11 +1,13 @@
 <script type="text/html" id="loggedin">
 	<div class="logged-in">
-		<h1>Success!</h1>
-        <h2>You are successfully logged in =)</h2>
-		<span class="session-id">
-			Your session id is:
-			<br/>
-			<%=sessionId%>
-		</span>
+		<div class="logged-in-window">
+			<h1>Success!</h1>
+			<h2>You are successfully logged in =)</h2>
+			<span class="session-id">
+				Your session id is:
+				<br/>
+				<%=sessionId%>
+			</span>
+		</div>
 	</div>
 </script>
